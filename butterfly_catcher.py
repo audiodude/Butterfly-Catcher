9d374a211d3224d49440f2a2e8c9b93234a46fea
Travis Briggs <briggs.travis@gmail.com>
2011-05-27 18:50:03 -0400

    First try at bouncing/escape code. Capturing is temporarily disabled for testing purposes.

--- butterfly_catcher.py
+++ butterfly_catcher.py
@@ -18,12 +18,40 @@ class RedBrick(pygame.sprite.Sprite):
     self.speed = 10
     self.direction = random.choice((-1,1))
 
+  # The paddle smashed me against one of the sides!
+  def squish(self):
+    self.kill()
+
+  # The paddle hit me, escape!
+  def escape(self, paddle):
+    self.direction *= -1
+    while Rect.colliderect(self.rect, paddle.rect):
+      relative_pos = self.rect.centery - paddle.rect.centery
+      if relative_pos > 0:
+        # paddle is above me, move downwards
+        self.direction = 1
+        self.rect.move_ip(0, self.direction*paddle.rect.height)
+        if self.rect.bottom >= SCREEN_RECT.bottom:
+          self.squish()
+
+      elif relative_pos < 0:
+        # paddle is below me, move upwards
+        self.direction = -1
+        self.rect.move_ip(0, self.direction*paddle.rect.height)
+        if self.rect.top <= SCREEN_RECT.top:
+          self.squish()
+          
+      elif relative_pos == 0:
+        #TODO: figure out what to do here, for now, just move up 1 px to break the tie
+        self.rect.move_ip(0, 1)
+      
   def update(self):
     self.rect.move_ip(0, self.speed*self.direction)
+        
     if self.rect.bottom >= SCREEN_RECT.bottom or self.rect.top <= SCREEN_RECT.top:
       self.direction *= -1
 
-class BlueBrick(pygame.sprite.Sprite):
+class Paddle(pygame.sprite.Sprite):
   LEFT, TOP, RIGHT, BOTTOM = 0, 1, 2, 3 
   
   def __init__(self, start_rect):
@@ -36,6 +64,7 @@ class BlueBrick(pygame.sprite.Sprite):
     self.images.insert(self.RIGHT, pygame.Surface((self.rect.height, self.rect.width)).convert())
     # self.images.insert(self.BOTTOM, pygame.Surface((self.rect.width, self.rect.height)).convert())
     
+    self.halves = list()
     for i, image in enumerate(self.images):
       sticky_half, bouncy_half = image.get_rect(), image.get_rect() #each call returns a new rect
             
@@ -61,6 +90,7 @@ class BlueBrick(pygame.sprite.Sprite):
 
       image.fill((0,0,255), sticky_half)
       image.fill((0,255,0), bouncy_half)
+      self.halves.insert(i, {'sticky_half':sticky_half, 'bouncy_half':bouncy_half})
 
     self.orientation = self.TOP
     self.image = self.images[self.orientation]
@@ -78,10 +108,12 @@ class BlueBrick(pygame.sprite.Sprite):
       self.orientation = 0
       return
       
-    print self.orientation
     self.rect.width, self.rect.height, self.rect.center = self.rect.height, self.rect.width, self.rect.center
     self.image = self.images[self.orientation]
 
+  def caught(self, red):
+    return False
+    
 def main():
   pygame.init()
   
@@ -99,23 +131,20 @@ def main():
   # Sprite groups
   all = pygame.sprite.RenderUpdates()
   red = pygame.sprite.Group()
-  blue = pygame.sprite.Group()
+  paddles = pygame.sprite.Group()
   
   # Ladies and Gentlemen, our Sprites!
   RedBrick.containers = all, red
   for i in range(4):
     RedBrick(Rect(5*PTX+(7*PTX*i), SCREEN_RECT.centery, PTX, PTX)) #recall that PTX is our made up unit
   
-  BlueBrick.containers = all, blue
-  BlueBrick(Rect(SCREEN_RECT.midbottom[0] - 2*PTX, SCREEN_RECT.midbottom[1] - 5*PTX, 4*PTX, PTX))
+  Paddle.containers = all, paddles
+  Paddle(Rect(SCREEN_RECT.midbottom[0] - 2*PTX, SCREEN_RECT.midbottom[1] - 5*PTX, 4*PTX, PTX))
+  velocity = [0, 0, 0, 0]
   
   # The clock
   clock = pygame.time.Clock()
 
-  # Blue brick movement
-  velocity = [0, 0, 0, 0]
-  ACCEL = 20
-  
   while True:
     clock.tick(60)
     
@@ -125,11 +154,11 @@ def main():
         elif event.type == KEYDOWN and event.key == K_ESCAPE:
             return
         elif event.type == KEYDOWN and (event.key == K_z or event.key == K_SPACE):
-          for b in blue.sprites():
-            b.flip(-1)
+          for p in paddles.sprites():
+            p.flip(-1)
         elif event.type == KEYDOWN and event.key == K_x:
-          for b in blue.sprites():
-            b.flip(1)
+          for p in paddles.sprites():
+            p.flip(1)
 
     # Modify velocity based on keypresses
     keystate = pygame.key.get_pressed()
@@ -143,8 +172,8 @@ def main():
         if velocity[idx] < 0:
           velocity[idx] = 0
                 
-    for b in blue.sprites():
-      b.move(velocity[0] - velocity[1], velocity[3] - velocity[2])
+    for p in paddles.sprites():
+      p.move(velocity[0] - velocity[1], velocity[3] - velocity[2])
     
     # clear/erase the last drawn sprites
     all.clear(screen, background)
@@ -152,8 +181,14 @@ def main():
     all.update()
     
     # Did any of the red bricks get captured?
-    for r in pygame.sprite.groupcollide(blue, red, 0, 1).values():
-      RedBrick(Rect(int(random.random()*SCREEN_RECT.width-PTX), SCREEN_RECT.centery, PTX, PTX).clamp(SCREEN_RECT))
+    for paddle, reds in pygame.sprite.groupcollide(paddles, red, 0, 0).iteritems():
+      for r in reds:         
+        if paddle.caught(r):
+          r.kill()
+          RedBrick(Rect(int(random.random()*SCREEN_RECT.width-PTX), SCREEN_RECT.centery, PTX, PTX).clamp(SCREEN_RECT))
+        else:
+          r.escape(paddle)
+          
 
     #draw the scene
     dirty = all.draw(screen)
